8940459f6a710edc7af1bf1f480f71bfb56dada2
Unknown <adrian.harenczyk@gmail.com>
2018-05-13 16:26:00 +0200

    More development with BoardValidator, added maximum and minimum board sizes.

--- src/main/java/ox/app/validators/BoardValidator.java
+++ src/main/java/ox/app/validators/BoardValidator.java
@@ -7,8 +7,10 @@ import java.util.function.Supplier;
 
 public class BoardValidator {
     private static final String VALIDATE_WIDTH_INFO = "Please pass desired board width\n" +
-                                    "(only integer higher than zero and lower than 51).";
+                                    "(only integer higher than 2 and lower than 101).";
     private static final String VALIDATE_HEIGHT_INFO = "Now please pass desired board height.";
+    private static final int MIN_BOARD_SIZE = 3;
+    private static final int MAX_BOARD_SIZE = 100;
 
     public static int validateHeight(Supplier<String> input, Consumer<String> output) {
         return validateAndAssign(VALIDATE_HEIGHT_INFO,input,output);
@@ -30,10 +32,14 @@ public class BoardValidator {
         }
     }
     private static int ifNumberAssign(String possibleNumber) throws WrongArgumentException{
-        if (InputIsNumberValidator.isNumber(possibleNumber)) {
+        int actualNumber;
+        if (InputIsNumberValidator.isNumber(possibleNumber) &&
+            (actualNumber = Integer.parseInt(possibleNumber)) <= MAX_BOARD_SIZE &&
+                actualNumber >= MIN_BOARD_SIZE) {
             return Integer.parseInt(possibleNumber);
         } else {
-            throw new WrongArgumentException("Passed text is not a number. Try again.");
+            throw new WrongArgumentException("Passed text is not an integer," +
+                    "\nor it's higher than 100 or lower than 3.\nTry again.");
         }
     }
 }
