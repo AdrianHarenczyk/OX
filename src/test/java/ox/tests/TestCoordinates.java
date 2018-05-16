501f9bad7c860aa6b2ca9e1ed10f2234b735cd52
Unknown <adrian.harenczyk@gmail.com>
2018-05-16 15:20:05 +0200

    More tests created.

---
+++ src/test/java/ox/tests/TestCoordinates.java
@@ -0,0 +1,30 @@
+package ox.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ox.app.game.Coordinate;
+
+public class TestCoordinates {
+
+    @Test
+    public static void ifCompareToWithSameValuesInCoordinatesReturnesZero() {
+        Coordinate coordinateWithValueTen = Coordinate.apply(10);
+        Coordinate coordinateWithSameValue = Coordinate.apply(10);
+        int zero = 0;
+        Assert.assertEquals(zero,coordinateWithValueTen.compareTo(coordinateWithSameValue));
+    }
+    @Test
+    public static void ifCompareToWithOtherValuesReturnsOneIfGreaterThan() {
+        Coordinate valueTen = Coordinate.apply(10);
+        Coordinate valueFifteen = Coordinate.apply(15);
+        int one = 1;
+        Assert.assertEquals(one,valueFifteen.compareTo(valueTen));
+    }
+    @Test
+    public static void ifCompareToWithOtherValuesReturnsMinusOneIfLessThan() {
+        Coordinate valueTen = Coordinate.apply(10);
+        Coordinate valueFifteen = Coordinate.apply(15);
+        int minusOne = -1;
+        Assert.assertEquals(minusOne,valueTen.compareTo(valueFifteen));
+    }
+}
